6e6f7819f3b9584cc324bb749d3c3827601b857d
Harsh Patel <harshppatel2880@gmail.com>
2020-10-16 13:51:13 -0500

    updated gitignore

---
+++ db/migrate/20201016184935_create_volumes.rb
@@ -0,0 +1,12 @@
+class CreateVolumes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :volumes do |t|
+      t.string :name
+      t.string :description
+      t.integer :start_year
+      t.string :site_detail_url
+
+      t.timestamps
+    end
+  end
+end
