d7e34e46f3da6d61b9b460a1b640cf37c8e306c2
Harsh Patel <harshppatel2880@gmail.com>
2020-10-16 14:04:19 -0500

    Created models and migrations for all tables

---
+++ app/models/issue.rb
@@ -0,0 +1,3 @@
+class Issue < ApplicationRecord
+  has_and_belongs_to_many :characters
+end
---
+++ db/migrate/20201016185326_create_issues.rb
@@ -0,0 +1,11 @@
+class CreateIssues < ActiveRecord::Migration[6.0]
+  def change
+    create_table :issues do |t|
+      t.string :name
+      t.string :description
+      t.string :site_detail_url
+
+      t.timestamps
+    end
+  end
+end
